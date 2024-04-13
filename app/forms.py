07d2b3684d3edfa6257ba714115151f3d4fca63d
MadhavanRamesh06 <maddyhereforu@gmail.com>
2024-04-13 15:20:58 +0530

    13 apr 2024

--- app/forms.py
+++ app/forms.py
@@ -3,12 +3,22 @@ from .models import Assignment,Course
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ['course_name', 'instructor_name', 'description', 'semester']
+        
+    def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['course_name'].widget.attrs.update({'class': 'form-control'})
+            self.fields['instructor_name'].widget.attrs.update({'class': 'form-control'})
+            self.fields['description'].widget.attrs.update({'class': 'form-control'})
+            self.fields['semester'].widget.attrs.update({'class': 'form-control'})
 
 class AssignmentForm(forms.ModelForm):
     class Meta:
         model = Assignment
         fields = ['course', 'assignment_name', 'due_date', 'description']
-    
         widgets = {
             'due_date': forms.DateInput(attrs={'type': 'date'})
         }
@@ -33,14 +43,3 @@ class SignUpForm(UserCreationForm):
             self.fields['password1'].widget.attrs.update({'class': 'form-control'})
             self.fields['password2'].widget.attrs.update({'class': 'form-control'})
 
-class CourseForm(forms.ModelForm):
-    class Meta:
-        model = Course
-        fields = ['course_name', 'instructor_name', 'description', 'semester']
-        
-    def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.fields['course_name'].widget.attrs.update({'class': 'form-control'})
-            self.fields['instructor_name'].widget.attrs.update({'class': 'form-control'})
-            self.fields['description'].widget.attrs.update({'class': 'form-control'})
-            self.fields['semester'].widget.attrs.update({'class': 'form-control'})
